db041ea53d5a784f2069c290253f5bd52ddfd004
Valentina Ventel <ventel.valentina@gmail.com>
2024-08-14 18:37:21 +0300

    Initial Commit

---
+++ Expenses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expenses
+//
+//  Created by Valentina Vențel on 14.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
